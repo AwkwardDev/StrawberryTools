759a56e483074f0e8ecae42444592df9833baa63
ProjectVoragine <dark.damphir@hotmail.com>
2011-10-13 23:08:13 -0500

    Added Source and Compiled Exe for the rest of the Strawberry Tools

---
+++ Source/SilinoronParser/SilinoronParser/bin/Release/parser pkt.bat
@@ -0,0 +1,3 @@
+@echo off
+SilinoronParser.exe -file 14333_2011-08-01_15-32-48_0030BDB8.pkt -loader pkt -tosql true
+PAUSE
---
+++ Source/SilinoronParser/SilinoronParser/bin/Release/parser.bat
@@ -0,0 +1,3 @@
+@echo off
+SilinoronParser.exe -file PacketDump.87851750 -loader Fabi -tosql true
+PAUSE
